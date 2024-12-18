5ab9cf2b79d1a4df28cadc04a563b3c23a2902eb
benyaminkristiawan <benyaminka2002@gmail.com>
2024-12-18 17:51:41 +0700

    update

--- js/script.js
+++ js/script.js
@@ -101,28 +101,28 @@ document.addEventListener('DOMContentLoaded', function() {
 sr.reveal('.animate-left', {
     origin: 'left',
     duration: 1000,
-    distance: '25rem',
+    distance: '1rem',
     delay: 300
 });
 
 sr.reveal('.animate-right', {
     origin: 'right',
     duration: 1000,
-    distance: '25rem',
+    distance: '1rem',
     delay: 600
 });
 
 sr.reveal('.animate-top', {
     origin: 'top',
     duration: 1000,
-    distance: '25rem',
+    distance: '1rem',
     delay: 600
 });
 
 sr.reveal('.animate-bottom', {
     origin: 'bottom',
     duration: 1000,
-    distance: '25rem',
+    distance: '1rem',
     delay: 600
 });
 });
